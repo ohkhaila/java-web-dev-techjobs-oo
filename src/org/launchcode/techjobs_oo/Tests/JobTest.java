959c1fe82432110910b6d3bf79b28db9907e1996
ohkhaila <khaila.jones@lessannoyingcrm.com>
2020-06-15 21:31:49 -0500

    Step 4 complete - Unit Testing

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -2,19 +2,25 @@ package org.launchcode.techjobs_oo.Tests;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.launchcode.techjobs_oo.Job;
+import org.launchcode.techjobs_oo.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class JobTest {
     Job carpenter;
     Job librarian;
+    Job productTester;
+    Job falseEqualJob1;
+    Job falseEqualJob2;
     @Before
     public void newJobs() {
         carpenter = new Job();
         librarian = new Job();
+        productTester = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        falseEqualJob1 = new Job("Unicorn Dev with no work life balance", new Employer("Drain a Dev"), new Location("burnt out town"), new PositionType("IT"), new CoreCompetency("Overworking"));
+        falseEqualJob2 = new Job("Unicorn Dev with no work life balance", new Employer("Drain a Dev"), new Location("burnt out town"), new PositionType("IT"), new CoreCompetency("Overworking"));
     }
+
     @Test
     public void testSettingJobId(){
         assertEquals((carpenter.getId()+ 1), librarian.getId());
@@ -24,5 +30,19 @@ public class JobTest {
     public void emptyTest(){
         assertEquals(10, 10, .001);
     }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        assertEquals("Product tester", productTester.getName());
+        assertEquals("ACME", productTester.getEmployer().getValue());
+        assertEquals("Desert", productTester.getLocation().getValue());
+        assertEquals("Quality control", productTester.getPositionType().getValue());
+        assertEquals("Persistence", productTester.getCoreCompetency().getValue());
+    }
+
+    @Test
+    public void testJobsForEquality(){ assertFalse(falseEqualJob1.equals(falseEqualJob2));}
+
+
 
 }
